97aaabc33058c67cbe29527f1a1b2ab31f549c2e
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-11-11 03:15:26 +0200

    task-5, branches

--- practice-1.js
+++ practice-1.js
@@ -9,8 +9,8 @@ console.log(`значение: ${a} тип данных  ${typeof a}`);
 const num = 33;
 console.log(`значение: ${num} тип данных ${typeof num}`);
 
-const country = 'Ukraine';
-console.log(`значение:${country} тип данных: ${typeof country}`);
+const country = "Ukraine";
+console.log(`значение:${country}тип данных: ${typeof country}`);
 
 let isOnline = true;
 console.log(`значение: ${isOnline} тип данных ${typeof isOnline}`);
@@ -18,64 +18,114 @@ console.log(`значение: ${isOnline} тип данных ${typeof isOnline
 let isOffLine = false;
 console.log(`значение: ${isOffLine} тип данных: ${typeof isOffLine} `);
 
-let password = 'qweqwe321';
+let password = "qweqwe321";
 console.log(`значение: ${password} тип данных: ${typeof password}`);
 
 // -------------------- task-2 -----------------------------
 
-const message = undefined;
-if (message === undefined) {
-  console.log(`тип данных прошел проверку`);
+let message;
+if (typeof x === "undefined") {
+  message = `тип данных прошел проверку`;
 } else {
-  console.log(`что-то пошло не так`);
+  message = `что-то пошло не так`;
 }
 console.log(message);
 
-const message1 = null;
-if (message1 !== null) {
-  console.log(`что-то пошло не так`);
+if (typeof a ==='object') {
+  message = `что-то пошло не так`;
 } else {
-  console.log(`тип данных прошел проверку`);
+  message = `тип данных прошел проверку`;
 }
-console.log(message1);
+console.log(message);
 
-const message2 = Number();
-if (message2 === NaN) {
-  console.log(`что-то пошло не так`);
+if (typeof num === "number") {
+  message = `что-то пошло не так`;
 } else {
-  console.log(`тип данных прошел проверку`);
+  message = `тип данных прошел проверку`;
 }
-console.log(typeof message2);
+console.log(message);
 
-const message3 = 'Ukraine';
-if (message3 === Number()) {
-    console.log(`тип данных прошел проверку`);
+if (typeof country === "string") {
+  message = `тип данных прошел проверку`;
 } else {
-    console.log(`что-то пошло не так`);
+  message = `что-то пошло не так`;
 }
-console.log( typeof message3);
+console.log(message);
 
-// isOnline = true;
-// if (isOnline === true) {
-//   alert(`тип данных прошел проверку`);
-// } else {
-
-//   alert(`что-то пошло не так`);
-// }
-// console.log(typeof isOnline);
-
-// isOffLine = false;
-// if (isOffLine === true) {
-//   alert(`что-то пошло не так`);
-// } else {
-//     alert(`тип данных прошел проверку`);
-// }
-// console.log(typeof isOffLine);
-
-password = prompt('введите пароль');
-if (password === 'qweqwe321') {
-  alert('парол принят, захады)');
+if (typeof isOnline === "boolean") {
+  message = `тип данных прошел проверку`;
 } else {
-  alert('извини брат, невэрный парол');
+  message = `что-то пошло не так`;
 }
-console.log(typeof password);
+console.log(message);
+
+if (typeof isOffLine === "boolean") {
+  message = `что-то пошло не так`;
+} else {
+  message = `тип данных прошел проверку`;
+}
+console.log(message);
+
+if (typeof password === "string") {
+  message = "парол принят, захады";
+} else {
+  message = "извини брат, невэрный парол";
+}
+console.log(message);
+
+// ----------------------------------------------------------
+
+let someNumber = !!55; //приведение числа к boolean
+console.log(typeof someNumber);
+
+// -------------------- task-3 -----------------------------
+
+let z = 10;
+if (z === 10) {
+  console.log(`Верно`);
+} else {
+  console.log(`Не верно`);
+}
+// -------------------- task-4 -----------------------------
+
+const min = 13;
+if (min >= 0 && min <= 15) {
+  console.log(`первая четверть`);
+} else if (min >= 16 && min <= 30) {
+  console.log(`вторая четверть`);
+} else if (min >= 31 && min <= 45) {
+  console.log(`третья четверть`);
+} else {
+  console.log(`четвертая четверть`);
+}
+// -------------------- task-5 -----------------------------
+
+let lang = "en";
+let arr = [];
+if (lang === "en") {
+  arr = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+} else if (lang === "ru") {
+  arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+} else {
+  console.log(`choose another language`);
+}
+console.log(arr);
+
+switch (lang) {
+  case "en":
+    arr = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    break;
+  case "ru":
+    arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+    break;
+  default:
+    console.log(`choose another language`);
+}
+
+
+const obj = {
+  'en': ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
+  'ru':  ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
+}
+console.log(obj[lang]);
+
