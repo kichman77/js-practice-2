f063cd27b663be50045b81a98e292dbfadf8c936
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-11-13 01:58:04 +0200

    methods array

--- practice-1.js
+++ practice-1.js
@@ -130,46 +130,163 @@ console.log(obj[lang]);
 
 // ----------------------------------------------------------
 
+const target2 = 3;
+let sum2 = 0;
+for (let i = 0; i < target2; i += 1) {
+  console.log(i);
+  sum2 += i;
+}
+console.log(sum2);
+
+const numberY = 10;
+for (let i = 0; i < numberY; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
+
 const numberX = "25";
 let sum = 0;
-for (let i = 0; i < numberX; i += 1) {
+for (let i = 0; i < numberX; i++) {
+  console.log("i", i);
   sum += i;
+  console.log("sum", sum);
 }
-console.log(sum);
+console.log(sum); // 300
 
 const target1 = 15;
 let sum1 = 0;
 
 for (let i = 0; i < target1; i += 1) {
+  console.log("i", i);
   sum1 += i;
+  console.log(`${i + 1} iteration sum:`, sum1);
 }
 
-console.log(sum1);
+// ------------------------------------------------
 
-const target2 = 3;
-let sum2 = 0;
+// перебор массива имен/ поиск нужного имени
 
-for (let i = 0; i < target2; i += 1) {
-  sum2 += i;
+const friends = ["Mango", "Kiwy", "Ajax", "Poly"];
+const findeName = "Ajax";
+let showMessage = "Don`t finde name";
+
+for (let i = 0; i < friends.length; i++) {
+  console.log("friends name ", friends[i]); // перебор и вывод в консоль
 }
 
-console.log(sum2);
+friends[4] = "Kongo"; //замена элемента
+console.log(friends);
+friends[0] = "Moty"; // добавление элемента
+console.log(friends.length);
 
-const target3 = 2;
-let sum3 = 0;
-
-for (let i = 0; i < target3; i += 1) {
-  sum3 += i;
+for (const friend of friends) {
+  if (findeName === friend) {
+    showMessage = "finde name";
+    break;
+  }
 }
 
-console.log(sum3);
+console.log(showMessage);
+
+// многомерный массив //
+
+const table = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(table[2][2]);
+console.log(table[0][1]);
+console.log(table.length);
+
+// ------------------// методы массива //-----------------------------
+
+const message7 = "Welcome to Kyiv";
+console.log(message7.split("")); // split()
+console.log(message7.split(" ")); // split()
+
+console.log(friends.join("")); //join()
+console.log(friends.join(",")); //join()
+console.log(friends.join("-")); //join()
+
+const friends1 = ["Mango", "Kiwy", "Ajax", "Poly"];
+console.log(friends1.indexOf("Ajax")); // indexOf() - выводит в консоль идекс элемента
+
+console.log(friends1.includes("Kiwy")); // true()
+console.log(friends1.includes("Moty")); // false()
+
+const redFruits = ["apple", "strawberry", "chery", "cranberries"];
+const fruit = "chery"; // includes()
+
+if (redFruits.includes("chery")) {
+  console.log("it is red fruit!"); // includes()
+}
+
+const clients = ["client1", "client2"];
+console.log(clients);
+
+console.log(clients.push("client3")); // push()
+console.log(clients.push("client4"));
+console.log(clients);
+
+console.log(clients.pop()); // pop()
+console.log(clients.pop());
+console.log(clients);
+
+console.log(clients.shift());
+clients.unshift("client0");
+console.log(clients);
+
+// --------------------------------------------
+
+const cars = [
+  {
+    id: 1,
+    name: "bmw",
+    value: 5,
+  },
+  {
+    id: 2,
+    name: "audi",
+    value: 3,
+  },
+  {
+    id: 3,
+    name: "mersedes",
+    value: 2,
+  },
+];
+console.log(cars);
+
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].id !== 2) continue;
+
+  console.log(cars[i]);
+}
+
+for (let car of cars) {
+  console.log(car);
+  if (car.value !== 5) continue;
+
+  console.log(car); //for of -  нахрен никому не нужен!!!!!!!
+}
+
+cars.map((el) => {
+  console.log(el);
+});
+
+// splice()
+const languages = ["C", "C++", "Java", "JavaScript"];
+console.log('languages',languages);// исходный массив
+//удаление
+const c = languages.splice(1, 2);
+console.log('c',c);
+console.log('languages',languages);
+//добавление с заменой и без
+const b = c.splice(1, 1, "C#", "Swift",'Go','Python');
+console.log('c',c);
+console.log('b',b);// обязательно удаление элемента
 
 
-const numberY = 10;
- for(let i = 0; i < numberY; i++ ){
-   if(i % 2 === 0){
-
-     continue
-   }
-   console.log(i);
- }
