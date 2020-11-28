b955a1b2c84060aa4523ba46dc037906abbda4e6
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-11-28 15:12:47 +0200

    some thing wrong

--- dom.js
+++ dom.js
@@ -31,6 +31,39 @@ import items from "./data/items.js";
 // const body = document.querySelector("body");
 // console.log(body);
 // titleRef.insertAdjacentElement("afterend", title);
+// -----------------------------------------------
+// const divTest = document.querySelector(".test");
+// // console.log(divTest);
+
+// const titleRef = document.createElement("h3");
+// titleRef.textContent = "Test";
+// titleRef.classList.add("title-test");
+// // console.log(titleRef);
+
+// const descTest = document.createElement("p");
+// descTest.textContent =
+//   "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, illo!";
+// descTest.classList.add("text");
+// // console.log(descTest);
+
+// const listTest = document.createElement("ul");
+// // console.log(listTest);
+// const itemTest = document.createElement("li");
+// itemTest.textContent = "testing Item";
+// // console.log(itemTest);
+// const itemTest1 = document.createElement("li");
+// itemTest1.textContent = "testing Item1";
+
+// const itemTest2 = document.createElement("li");
+// itemTest2.textContent = "testing Item2";
+
+// const itemTest3 = document.createElement("li");
+// itemTest3.textContent = "testing Item3";
+// listTest.append(itemTest, itemTest1, itemTest2, itemTest3);
+// // console.log(listTest);
+
+// divTest.append(titleRef, descTest, listTest);
+// console.log(divTest);
 
 // -------------------------------------
 
@@ -44,3 +77,11 @@ function createElement(array) {
   console.log(array);
 }
 createElement(items);
+
+// console.log(items[0]);
+
+items.forEach(item => {
+  console.log(createElement(item));
+});
+const produtCards = items.map(item => createElement(item))
+console.log(produtCards);
